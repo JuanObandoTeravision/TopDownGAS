359eff6230490fb5ddb7450831eb6464d128b739
JuanObandoTG <obando@teravisiongames.com>
2024-02-09 16:10:45 -0500

    S1: Enhanced Input

---
+++ Source/Aura/Private/Player/AUR_PlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Teravision Games
+
+
+#include "Player/AUR_PlayerController.h"
+
---
+++ Source/Aura/Public/Player/AUR_PlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Teravision Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AUR_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAUR_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
