72d46d1b0ecc23bbdcfd1efa00ee8139a89bc6fa
JuanObandoTG <obando@teravisiongames.com>
2024-02-12 12:25:22 -0500

    Aura player controller

--- Source/Aura/Private/Player/AUR_PlayerController.cpp
+++ Source/Aura/Private/Player/AUR_PlayerController.cpp
@@ -3,3 +3,27 @@
 
 #include "Player/AUR_PlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+
+AAUR_PlayerController::AAUR_PlayerController()
+{
+	bReplicates = true;
+}
+
+void AAUR_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/Aura/Public/Player/AUR_PlayerController.h
+++ Source/Aura/Public/Player/AUR_PlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "AUR_PlayerController.generated.h"
 
+
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -13,5 +16,14 @@ UCLASS()
 class AURA_API AAUR_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+public:
+	AAUR_PlayerController();
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 	
 };
