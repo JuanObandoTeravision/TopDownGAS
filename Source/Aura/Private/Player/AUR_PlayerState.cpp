6a521d9877bd0b7c5fb0c2c82946e1220ce67bea
JuanObandoTG <obando@teravisiongames.com>
2024-02-15 11:14:23 -0500

    S3: The Player State

---
+++ Source/Aura/Private/Player/AUR_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Teravision Games
+
+
+#include "Player/AUR_PlayerState.h"
+
+AAUR_PlayerState::AAUR_PlayerState()
+{
+	NetUpdateFrequency = 100.0f;
+}
---
+++ Source/Aura/Public/Player/AUR_PlayerState.h
@@ -0,0 +1,20 @@
+// Copyright Teravision Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AUR_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAUR_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AAUR_PlayerState();
+	
+};
