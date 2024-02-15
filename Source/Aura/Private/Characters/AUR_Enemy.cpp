5ccb7962ca175b5cdc52c7ae5dec5534ce175ec1
JuanObandoTG <obando@teravisiongames.com>
2024-02-15 10:38:21 -0500

    S2: Post Process Highlight

--- Source/Aura/Private/Characters/AUR_Enemy.cpp
+++ Source/Aura/Private/Characters/AUR_Enemy.cpp
@@ -2,23 +2,32 @@
 
 
 #include "Characters/AUR_Enemy.h"
+
+#include "Aura/Aura.h"
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 AAUR_Enemy::AAUR_Enemy()
 {
 	bIsHighlighted = false;
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void AAUR_Enemy::HighlightActor()
 {
 	bIsHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 void AAUR_Enemy::UnHighlightActor()
 {
 	bIsHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
