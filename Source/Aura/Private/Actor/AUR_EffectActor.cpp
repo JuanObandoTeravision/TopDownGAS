06e99ca13c29bb305ef14534cfc5123fd4ca64a0
JuanObandoTG <obando@teravisiongames.com>
2024-03-04 10:53:19 -0500

    S6: Effect actor improved

--- Source/Aura/Private/Actor/AUR_EffectActor.cpp
+++ Source/Aura/Private/Actor/AUR_EffectActor.cpp
@@ -3,47 +3,46 @@
 
 #include "Actor/AUR_EffectActor.h"
 
-#include "AbilitySystemInterface.h"
-#include "AUR_AttributeSet.h"
-#include "Components/SphereComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
 
 // Sets default values
 AAUR_EffectActor::AAUR_EffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
-
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	
 }
 
 void AAUR_EffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAUR_EffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAUR_EffectActor::EndOverlap);
 }
 
-void AAUR_EffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void AAUR_EffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UAUR_AttributeSet* AuraAttributeSet = Cast<UAUR_AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UAUR_AttributeSet::StaticClass()));
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if (TargetASC == nullptr) return;
 
-		UAUR_AttributeSet* MutableAuraAttributeSet = const_cast<UAUR_AttributeSet*>(AuraAttributeSet);
-		MutableAuraAttributeSet->SetMana(AuraAttributeSet->GetHealth() - 25.f);
-		Destroy();
-	}
-}
+	check(GameplayEffectClass);
+	
+	/**
+	 * Specs first need a context to work. The Context Handle is just a wrapper that contains an internal gameplay effect context, that is,
+	 * important information regarding the effect, such as source, target, etc. Every ASC can make a context, then you can add the appropiate info 
+	 */
+	
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
 
-void AAUR_EffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
+	/**
+	 * Remember, a spec is just a optimized version of a gameplay effect. A spec handle  will contain a TSharedPtr to the spec itself. It needs the context
+	 * to knowledge of important information of the spec. You can also specify the spec level.
+	 */
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
 
+	/**
+	 * We de-reference the tshareptr because the function receives a FGameplayEffectSpec by reference, not the handle. The handle contains a ptr to the spec
+	 */
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
--- Source/Aura/Public/Actor/AUR_EffectActor.h
+++ Source/Aura/Public/Actor/AUR_EffectActor.h
@@ -8,6 +8,7 @@
 
 class UStaticMeshComponent;
 class USphereComponent;
+class UGameplayEffect;
 UCLASS()
 class AURA_API AAUR_EffectActor : public AActor
 {
@@ -16,18 +17,13 @@ class AURA_API AAUR_EffectActor : public AActor
 public:	
 	AAUR_EffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	virtual void BeginPlay() override;
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 };
