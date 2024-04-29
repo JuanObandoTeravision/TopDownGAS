5f8e64a5bd2ccc212310faf550a31c665528d727
JuanObandoTG <obando@teravisiongames.com>
2024-04-29 09:55:19 -0500

    S9: Attribute Info Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/AUR_AttributeInfo.cpp
@@ -0,0 +1,22 @@
+// Copyright Teravision Games
+
+
+#include "AbilitySystem/Data/AUR_AttributeInfo.h"
+
+FAuraAttributeInfo UAUR_AttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInformation)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(),*GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
